ecc9ac99cadb846c65fa8445e850cb0b9173fe0e
izabelaoska <91794571+izabelaoska@users.noreply.github.com>
2022-09-26 15:03:37 +0100

    chore: checkout page functionality

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -35,19 +35,28 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   )
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+  return cartItems.filter(
+    (cartItem) => cartItem.id !== cartItemToClear.id
+  )
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 })
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
   const [cartCount, setCartCount] = useState(0)
+  const [cartTotal, setCartTotal] = useState(0)
 
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -55,6 +64,15 @@ export const CartProvider = ({ children }) => {
       0
     )
     setCartCount(newCartCount)
+  }, [cartItems])
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) =>
+        total + cartItem.quantity * cartItem.price,
+      0
+    )
+    setCartTotal(newCartTotal)
   }, [cartItems])
 
   const addItemToCart = (productToAdd) => {
@@ -65,13 +83,18 @@ export const CartProvider = ({ children }) => {
       removeCartItem(cartItems, cartItemToRemove)
     )
   }
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear))
+  }
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
     removeItemFromCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   }
   return (
     <CartContext.Provider value={value}>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,31 +4,39 @@ import "./checkout.styles.scss"
 import { useContext } from "react"
 
 import { CartContext } from "../../context/cart.context"
+import CheckoutItem from "../../components/checkout-item/checkout-item.component"
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemFromCart } =
-    useContext(CartContext)
+  const { cartItems, cartTotal } = useContext(CartContext)
 
   return (
-    <div>
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quatity </span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
       {cartItems.map((cartItem) => {
-        const { id, name, quantity } = cartItem
         return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
-            <span onClick={() => addItemToCart(cartItem)}>
-              increment
-            </span>
-            <br />
-            <span
-              onClick={() => removeItemFromCart(cartItem)}
-            >
-              decrement
-            </span>
-          </div>
+          <CheckoutItem
+            key={cartItem.id}
+            cartItem={cartItem}
+          />
         )
       })}
+      <span className="total">Total : £{cartTotal}</span>
     </div>
   )
 }
